f3aae18cdf699b2358e0f383de9ab5896dc364a6
nur <medo0o6665@gmail.com>
2022-06-24 18:04:48 +0800

    create landing page

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Landing = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+      }}
+    >
+      <button>Start app</button>
+    </div>
+  );
+};
