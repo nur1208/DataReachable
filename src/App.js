772debe258e80f455fee50fd3baee9134ddfa621
nur <medo0o6665@gmail.com>
2022-06-24 18:19:34 +0800

    add react-route-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import Test from "./components/Test";
 import { Landing } from "./pages/Landing";
-
+import { Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Landing />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/about" element={<div>about</div>} />
+        <Route path="/users" element={<div>users</div>} />
+        <Route path="/" element={<div>Home</div>} />
+      </Routes>
     </div>
   );
 }
