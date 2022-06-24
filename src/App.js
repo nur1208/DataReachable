1bbc06eb69bf6bb0c808747dd8e490ffb3d42881
nur <medo0o6665@gmail.com>
2022-06-24 20:56:08 +0800

    create InputArray component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import Test from "./components/Test";
 import { Landing } from "./pages/Landing";
 import { Routes, Route, Link } from "react-router-dom";
+import InputArray from "./components/InputArray";
 function App() {
   return (
     <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
-        </ul>
-      </nav>
       <Routes>
         <Route path="/about" element={<div>about</div>} />
         <Route path="/users" element={<div>users</div>} />
+        <Route path="/test" element={<InputArray />} />
         <Route path="/" element={<div>Home</div>} />
       </Routes>
     </div>
