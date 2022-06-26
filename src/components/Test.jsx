87298114a9b3a66855fe3ed2e8195dd7595c3d2a
nur <medo0o6665@gmail.com>
2022-06-26 13:41:33 +0800

    Test Notification component in Test component

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactLoading from "react-loading";
 import { MainLadingWrapper } from "../pages/Landing/Landing.styles";
+import colors from "../styles/colors";
+import { useNotification } from "./Notification";
 
-export const Test = ({ type, color }) => (
-  <MainLadingWrapper>
-    <ReactLoading
-      type={type}
-      color={color}
-      height={300}
-      width={200}
-    />
-  </MainLadingWrapper>
-);
+export const Test = ({ type, color }) => {
+  const dispatch = useNotification();
+
+  useEffect(() => {
+    dispatch({
+      type: "success",
+      message: `Nice! You are going to !!`,
+      title: "Booking Successfully",
+      position: "topL",
+      iconColor: colors.green,
+    });
+  }, []);
+
+  return (
+    <MainLadingWrapper>
+      <ReactLoading
+        type={type}
+        color={color}
+        height={300}
+        width={200}
+      />
+    </MainLadingWrapper>
+  );
+};
